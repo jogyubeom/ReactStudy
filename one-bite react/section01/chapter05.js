5f5ad9e9dcf7a0ed1adb933eb6c6132ccfc02f78
조규범 <standardjo@naver.com>
2024-08-27 15:39:51 +0900

    0827ssafy

---
+++ one-bite react/section01/chapter05.js
@@ -0,0 +1,28 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+// 2. String Type
+let myName = "이정환";
+let myLocation = "목동";
+let introduce = myName + myLocation; // "이정환목동"
+
+// 템플릿 리터럴 문법
+let introduceText = `${myName}은 ${myLocation}에 거주합니다`;
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다, 프로그래머가 의도적으로 선언함)
+let empty = null;
+
+// 5. Undefined Type (아무것도 없다, 자동으로 시스템이 말해줌)
+let none;
+console.log(none);
